8884e19b8d064480b76b6c4d1c5291dd8a7e7b94
Zythee3 <zmatheusguilherme2@gmail.com>
2024-05-04 18:41:51 -0300

    update

--- src/components/TelaInicial/TelaInicial.jsx
+++ src/components/TelaInicial/TelaInicial.jsx
@@ -1,67 +1,73 @@
 import React from 'react'
-import "./elementos_tela_inicial.css"
+import "./Tela_inicial.css"
+import Cards from "../Cards/Cards"
 
-const tela_inicial = () => {
+const TelaInicial = () => {
     return (
-        <div>
-            <div class="div_pai">
-                <div class = "div_barra_principal">
-                    <div class = "logo">
-                        <img class = "imagem" src="https://conecta.vivendocfit.com/assets/logo_fav-a853b0e2.svg"/>
+
+        <div class="div_pai">
+            <div class="div_pai_navbar">
+                <div class="div_barra_principal">
+                    <div>
+                        <img class="imagem" src="https://conecta.vivendocfit.com/assets/logo_fav-a853b0e2.svg" />
                     </div>
-                    <div class = "nome_usuario">
+                    <div class="nome_usuario">
                         <p class="nome">
                             Olá, MATHEUS GUILHERME
                         </p>
                         <p class="bora_semexer">
-                        Bora se mexer?
+                            Bora se mexer?
                         </p>
                     </div>
-                    <div class="container_lateral">
-                        <div class = "botao_lateral">
-                            <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" focusable="false" class="tamanho_botao_lateral" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
-                        </div>
+                </div>
+                <div class="container_lateral">
+                    <div class="botao_lateral">
+                        <svg stroke="currentColor" fill="none" stroke-width="2" viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round" focusable="false" class="tamanho_botao_lateral" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                     </div>
                 </div>
-            
+
+            </div>
+                <div className="container">
+
+                
                 <div class="barra_pesquisa">
-                    <svg class = "barra_icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m229.66 218.34l-50.07-50.06a88.11 88.11 0 1 0-11.31 11.31l50.06 50.07a8 8 0 0 0 11.32-11.32M40 112a72 72 0 1 1 72 72a72.08 72.08 0 0 1-72-72"/></svg>
-                    <input class="input_pesquisa" type="text" placeholder='Pesquisar Eventos'  />
+                    <svg class="barra_icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 256 256"><path fill="currentColor" d="m229.66 218.34l-50.07-50.06a88.11 88.11 0 1 0-11.31 11.31l50.06 50.07a8 8 0 0 0 11.32-11.32M40 112a72 72 0 1 1 72 72a72.08 72.08 0 0 1-72-72" /></svg>
+                    <input class="input_pesquisa" type="text" placeholder='Pesquisar Eventos' />
 
                 </div>
-                
+
                 <div class="texto_esportes">
                     <b>Nossos esportes</b>
                 </div>
 
-                <div class = "menu_esportes">
-                    
-                    <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m14 7l-5 5l5 5"/></svg>
-                    
+                <div class="menu_esportes">
+
+                    <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m14 7l-5 5l5 5" /></svg>
+
                     <button>
-                        <img class ="imagem_esportes" src="https://conecta.vivendocfit.com/assets/ICON%20FUNCIONAL-34454444.svg"/>
-                        Funcional
+                        <img class="imagem_esportes" src="https://conecta.vivendocfit.com/assets/ICON%20FUNCIONAL-34454444.svg" />
+                        <span>Funcional</span>
                     </button>
-                    
-                    
+
+
+
                     <button>
                         <img class="imagem_esportes" src="https://conecta.vivendocfit.com/assets/ICON%20FUTEBOL-87a06d75.svg" />
-                        Futebol
+                        <span>Futebol</span>
                     </button>
 
                     <button>
-
                         <img class="imagem_esportes" src="https://conecta.vivendocfit.com/assets/ICON%20FUTSAL-e112da7a.svg" />
-                        Futsal
+                        <span>Futsal </span>
                     </button>
-                    
-                    
+
+
                     {/* <div class="esportes">
                         <img class="imagem_esportes" src="https://conecta.vivendocfit.com/assets/ICON%20OUTROS-fd8ee86d.svg"/>
                         Outros
                     </div> */}
-                    
-                    <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m10 17l5-5l-5-5"/></svg>
+
+                    <svg class="arrow" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="m10 17l5-5l-5-5" /></svg>
 
                 </div>
                 <div class="opcoes_eventos">
@@ -75,8 +81,13 @@ const tela_inicial = () => {
                         Criar Evento
                     </div>
                 </div>
-                
-                <div class="sessao_cards">
+                <div className='cards'>
+                    <Cards />
+                    <Cards />
+                    <Cards />
+                    <Cards />
+                </div>
+                {/* <div class="sessao_cards">
                     <button>
                         
                             <img class="img_cards" src="https://uploaddeimagens.com.br/images/004/527/556/full/FUNCIONAL.png?1688152616" />
@@ -187,19 +198,14 @@ const tela_inicial = () => {
                             </div>
                         </div>
                     </button>
+                
                     
-                   
-                   
-                    
+                </div> */}
+
+
                 </div>
-
-
-
             </div>
-
-
-        </div>
     )
 }
 
-export default tela_inicial
+export default TelaInicial
